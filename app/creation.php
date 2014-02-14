13fafb9cac151c3f5a1fd42c6f32a5762b0f7d7a
cherwood <robin.mastromarino@gmail.com>
2014-02-14 19:12:54 +0100

    First step
    step one

---
+++ app/creation.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Creation | Adventure chain</title>	
+		<meta charset="utf-8" />
+		<meta name="description" content="Adventure chain." />
+		<meta name="keywords" content="Adventure, chain" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="../font/font.css">
+		<link rel="stylesheet" type="text/css" href="../css/styles.css">
+		<link rel="icon" href="img/favicon.ico"/>
+		<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->	
+    
+	</head>
+	
+  <body>
+  
+  <div id="page">
+    <div id="creation">
+    
+      <h1>Creation</h1>
+      <h2>Choose your character's sex</h2>
+      
+      <div id="chooseBoy" class="choose">
+      
+        <a href="#">
+          <img src="../img/choose-boy.png" alt="choose a boy" />
+          <p>a boy</p>
+        </a>
+        
+      </div>
+      
+      
+      
+      <div id="chooseGirl" class="choose">
+        
+        <a href="#">
+          <img src="../img/choose-girl.png" alt="choose a girl" />
+          <p>a girl</p>
+        </a>
+      </div>
+      
+      <hr class="clear"/>
+      
+    </div><!-- *** END CREATION *** -->
+  </div><!-- *** END PAGE *** -->
+  
+  
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  </body>
+	 
+</html>
