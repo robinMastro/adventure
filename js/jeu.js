a2ec285ffc3a7428bdfdbf283b1815b67ba4399c
Gyncoca <benoitandrea@hotmail.fr>
2014-02-14 17:46:08 +0100

    Save + load
    Maintenant en changeant de page le score reste.

--- js/jeu.js
+++ js/jeu.js
@@ -8,7 +8,26 @@ function clickMoney(){
 
     var e = document.getElementById("score");
     e.innerHTML = player.score;
-
-
+    save_game();
 
 }
+
+function save_game() {
+   localStorage['SaveChain'] = btoa(JSON.stringify(player));
+}
+
+function load_game() {
+    if (!localStorage['SaveChain']) return;
+    var save_data = JSON.parse(atob(localStorage['SaveChain']));
+    player = save_data;
+    update_view();
+}
+
+function update_view(){
+	var e = document.getElementById("score");
+    e.innerHTML = player.score;
+
+}
+
+
+load_game();
