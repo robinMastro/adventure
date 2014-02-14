bd91a4be534717a7e6d9aad2cc23b82c56b4821a
cherwood <robin.mastromarino@gmail.com>
2014-02-14 15:47:24 +0100

    Adventure

---
+++ app/app.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>App Adventure chain</title>	
+		<meta charset="utf-8" />
+		<meta name="description" content="Adventure chain." />
+		<meta name="keywords" content="Adventure, chain" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="../font/font.css">
+		<link rel="stylesheet" type="text/css" href="../css/styles.css">
+		<link rel="icon" href="img/favicon.ico"/>
+		<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->	
+    
+	</head>
+	
+  <body>
+  
+  <h1>Adventure chain App</h1>
+  
+  <div onclick="clickMoney()" id="clickme"></div>
+
+  <p id="money">money</p>
+  
+  
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  </body>
+	 
+</html>
