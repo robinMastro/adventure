b593fcaa279c61b76aa2907cde466d560d30c6de
cherwood <robin.mastromarino@gmail.com>
2014-02-17 20:32:37 +0100

    addchain

---
+++ js/addchain.js
@@ -0,0 +1,26 @@
+function deselect(e) {
+    $(".pop").slideFadeToggle(function() {
+        e.removeClass("selected");
+    });    
+}
+
+$(function() {
+    $("#addchain").on('click', function() {
+        if($(this).hasClass("selected")) {
+            deselect($(this));               
+        } else {
+            $(this).addClass("selected");
+            $(".pop").slideFadeToggle();
+        }
+        return false;
+    });
+
+    $(".close").on('click', function() {
+        deselect($(this));
+        return false;
+    });
+});
+
+$.fn.slideFadeToggle = function(easing, callback) {
+    return this.animate({ opacity: 'toggle', height: 'toggle' }, "fast", easing, callback);
+};
