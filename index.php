19c78a34fab12a73bfeb2029fff73143cdd78992
cherwood <robin.mastromarino@gmail.com>
2014-02-17 04:05:44 +0100

    Home

--- index.php
+++ index.php
@@ -18,9 +18,93 @@
 	
   <body>
   
-  <h1>Adventure chain</h1>
-  
-  <a href="app/app.php">Get started now</a>
+    <div id="page" class="landing">
+      
+      <div id="header-landing">
+        <div class="wrapper-landing">
+        
+          <img class="hl-left" src="img/landing/header-personnage.png" alt="Adventure chain présentation" />
+          
+          <h1 class="hl-right"><img src="img/landing/logo-adventurechain.png" at="Logo Adventure chain" /></h1>
+          
+          <h2 class="hl-right">Build better habits</h2>
+          
+          <p class="hl-right"><strong>The fun way, one day at a time.</strong></p>
+          
+          <a class="hl-right" href="#">Get started now</a>
+          
+          <hr class="clear" />
+        
+        </div><!-- *** END WRAPPER LANDING *** -->
+      </div><!-- *** END HEADER LANDING *** -->
+      
+      <div id="work-landing">
+        <div class="wrapper-landing">
+        
+          <h2>How it works</h2>
+          
+          <p>Based on the method «Don’t Break the Chain», you get to decide an activity you’d like to do everyday, and the more days in a row you keep up with this habit, the more you get rewarded with XP points and freaking (virtual) cash.</p>
+          
+          <img id="work-img-chain" src="img/landing/work-chain.png" alt="Work chain" />
+          
+          <p><strong>Your goal here is to never break the chain.</strong></p>
+      
+        </div><!-- *** END WRAPPER LANDING *** -->
+      </div><!-- *** END WORK LANDING *** -->
+      
+      <div id="list-landing">
+        <div class="wrapper-landing">
+          
+          <ul>
+          
+            <li>
+              <h3>Push me everyday</h3>
+              <img src="img/landing/pushme.png" alt="Push me everyday" />
+              <p>Each day you achieve one of your predefined tasks, push the red button and feel joy.</p>
+            </li>
+            
+            <li>
+              <h3>Harder, better, cooler</h3>
+              <img src="img/landing/pushme.png" alt="Push me everyday" />
+              <p>Gradually incorporate good habits into your lifestyle; gain money and stylise your character the way you want !</p>
+            </li>
+            
+            <li>
+              <h3>Happiness <span>(KINDA)</span></h3>
+              <img src="img/landing/pushme.png" alt="Push me everyday" />
+              <p>Soon, become (virtually) rich and brag to your friends how good your in-game character looks.</p>
+            </li>
+            
+          </ul>
+          
+          <hr class="clear" />
+          
+        </div><!-- *** END WRAPPER LANDING *** -->
+      </div><!-- *** END LIST LANDING *** -->   
+      
+      <div id="easy-landing">
+        <div class="wrapper-landing">
+        
+          <img src="img/landing/easy-use.png" alt="esay use" />
+        
+          <h2>Easy as hell</h2>
+          <p>Simply choose your character, give them a name, set-up your habits  and you’re pretty much good to go.</p>
+      
+          <hr class="clear" />
+          
+        </div><!-- *** END WRAPPER LANDING *** -->
+      </div><!-- *** END EASY LANDING *** -->  
+      
+      <div id="start-landing">
+        <div class="wrapper-landing"> 
+        
+          <p><a href="#">Get started now</a></p>
+        
+        </div><!-- *** END WRAPPER LANDING *** -->
+      </div><!-- *** END START LANDING *** -->  
+       
+    
+    </div><!-- *** END PAGE *** -->
   
   
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
