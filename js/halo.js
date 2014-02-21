9d3e6590e509439519b516e9684eb82b5d31d8c3
cherwood <robin.mastromarino@gmail.com>
2014-02-21 05:57:43 +0100

    Update

---
+++ js/halo.js
@@ -0,0 +1,59 @@
+$(".shoes .shop-item").click(function(){
+        $(".shoes .shop-item").removeClass("item-current");
+    });
+    $(".shoes-1").click(function(){
+        $(".shoes-1").addClass("item-current");
+    });
+    $(".shoes-2").click(function(){
+        $(".shoes-2").addClass("item-current");
+    });
+    $(".shoes-3").click(function(){
+        $(".shoes-3").addClass("item-current");
+    });
+    $(".shoes-4").click(function(){
+        $(".shoes-4").addClass("item-current");
+    });
+    
+    $(".pants .shop-item").click(function(){
+        $(".pants .shop-item").removeClass("item-current");
+    });
+    $(".pants-1").click(function(){
+        $(".pants-1").addClass("item-current");
+    });
+    $(".pants-2").click(function(){
+        $(".pants-2").addClass("item-current");
+    });
+    $(".pants-3").click(function(){
+        $(".pants-3").addClass("item-current");
+    });
+    $(".pants-4").click(function(){
+        $(".pants-4").addClass("item-current");
+    });
+    $(".pants-5").click(function(){
+        $(".pants-5").addClass("item-current");
+    });
+    $(".pants-6").click(function(){
+        $(".pants-6").addClass("item-current");
+    });
+    
+    $(".top .shop-item").click(function(){
+        $(".top .shop-item").removeClass("item-current");
+    });
+    $(".top-1").click(function(){
+        $(".top-1").addClass("item-current");
+    });
+    $(".top-2").click(function(){
+        $(".top-2").addClass("item-current");
+    });
+    $(".top-3").click(function(){
+        $(".top-3").addClass("item-current");
+    });
+    $(".top-4").click(function(){
+        $(".top-4").addClass("item-current");
+    });
+    $(".top-5").click(function(){
+        $(".top-5").addClass("item-current");
+    });
+    $(".top-6").click(function(){
+        $(".top-6").addClass("item-current");
+    });
